96aa77f3acc4f0b989c9834349c1059d358bcbd2
joshua-v-jones <joshua.v.jones91@gmail.com>
2021-03-06 23:27:49 +0100

    Adding in the code to hide archived comments select box and perform the operation automatically when the drop down closes.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,11 @@
 class CommentsController < ApplicationController
     http_basic_authenticate_with name: "dhh", password: "secret", only: :destroy
+  def update
+	@article = Article.find(params[:article_id])
+	@comment = @article.comments.find(params[:id])
+	@comment.update(comment_status_params)
+    redirect_to article_path(@article)
+  end
 	def create
 		@article = Article.find(params[:article_id])
 		@comment = @article.comments.create(comment_params)
@@ -14,5 +20,8 @@ class CommentsController < ApplicationController
   private
 	def comment_params
 		params.require(:comment).permit(:commenter, :body, :status)
+	end
+	def comment_status_params
+		params.permit(:status)
 	end
 end
